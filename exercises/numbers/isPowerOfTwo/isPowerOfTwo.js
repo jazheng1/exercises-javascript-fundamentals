e129fd6b566c2e91e9ca14d869f67a16c379eca6
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-15 15:24:00 -0800

    Update isPowerOfTwo.js

--- exercises/numbers/isPowerOfTwo/isPowerOfTwo.js
+++ exercises/numbers/isPowerOfTwo/isPowerOfTwo.js
@@ -1,32 +1,50 @@
 /**
- * Given a non-negative integer as input, returns `true` if input is a power of `2` and `false` otherwise.
+ * Given two non-negative integers, `n` and `b`, return `true` of `n` is a power of `b`
+ * and `false` otherwise.
  *
- * A number is a power of `2` if it can be written as `2**k` for some positive integer `k`, where "`**`"
- * represents the exponentiation operator.
+ * A number is a `n` power of `b` if it can be written as `b**k` for some positive integer `k`,
+ * where "`**`" represents the exponentiation operator.
  *
  * For example, since `8` equals `2 * 2 * 2`, which is `2**3`, then `8` is a power of `2`.
- * Conversely, `81` is not a power of `2` since there's no integer `k` such that `81 === 2**k`.
- *
- * However, `81` _is_ a power of `3` since `81 === 3**4`.
+ * Conversely, `9` is not a power of `2` since there's no integer `k` such that `9 === 2**k`.
  *
  * @example
- * isPowerOf(2, 2) // => true
- * isPowerOf(3, 3) // => true
- * isPowerOf(10, 2) // => false
+ * isPowerOf(2) // => true
+ * isPowerOf(3) // => false
+ * isPowerOf(4) // => true
+ * isPowerOf(2**20) // => true
+ * isPowerOf(2**20 + 1) // => false
  *
- * @param {number} n - The number
- * @param {number} b - The base
- * @returns {boolean} True if the number is a power of the base, false otherwise.
+ * @param {number} num - The input number
+ * @returns {boolean} True if input is a power of 2 and false otherwise.
  */
 function isPowerOfTwo(num) {
   // Your code here
   // Remember, you can assume that num is a positive integer.
+  let b = 2;
+  if(num === 0){
+    return true;
+  }else{
+    while(num > b){
+      b = 2 * 2;
+    }
+  }
+  return b === num;
 }
+
 
 if (require.main === module) {
   console.log('Running sanity checks for isPowerOfTwo:');
 
-  // Your sanity checks here.
+  // Is 0 a power of two? Is 1?
+  console.log(isPowerOfTwo(0) === true);
+  console.log(isPowerOfTwo(1) === false);
+
+  console.log(isPowerOfTwo(2) === true);
+  console.log(isPowerOfTwo(3) === false);
+  console.log(isPowerOfTwo(4) === true);
+  console.log(isPowerOfTwo(16) === true);
 }
 
 module.exports = isPowerOfTwo;
+
