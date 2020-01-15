656d2a94f3c589d908badf28cf83f84efdde89e5
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-15 11:45:11 -0800

    Update remainderOf.js

--- exercises/numbers/remainderOf/remainderOf.js
+++ exercises/numbers/remainderOf/remainderOf.js
@@ -15,20 +15,34 @@
  * @param {number} num The input number
  * @returns {boolean} True if num is even and false otherwise
  */
-function remainderOf(num) {
-  /*
-    This is your job. :)
+function remainderOf(num1, num2) {
+    /*
+      This is your job. :)
+      If you're not sure, step out of the code and use pen + paper. Start
+      with simple examples and pay attention to the process you carry out.
+    */
+    let quotient = num1/num2;
+    let roundDown = Math.floor(quotient);
+    let remainder = (quotient - roundDown) * num2;
+    if((remainder - Math.floor(remainder)) > .5){
+        return Math.ceil(remainder);
+    }else{
+        return Math.floor(remainder);
+    }
 
-    If you're not sure, step out of the code and use pen + paper. Start
-    with simple examples and pay attention to the process you carry out.
-  */
-}
-
-if (require.main === module) {
-  console.log('Running sanity checks for remainderOf');
-
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
-}
-
-module.exports = remainderOf;
+  }
+  
+  if (require.main === module) {
+    console.log('Running sanity checks for remainderOf');
+  
+    // Add your own sanity checks here.
+    // How else will you be sure your code does what you think it does?
+    console.log(remainderOf(10, 1));
+    console.log(remainderOf(10, 4));
+    console.log(remainderOf(10, 3));
+    console.log(remainderOf(129, 17));
+    console.log(remainderOf(9, 2));
+    console.log(remainderOf(109, 15));
+  }
+  
+  module.exports = remainderOf;
