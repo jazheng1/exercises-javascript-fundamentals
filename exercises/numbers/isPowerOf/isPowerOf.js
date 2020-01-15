744e4dcf44cb1cdb6b6ac7c550d1470c944dba73
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-15 15:01:06 -0800

    Update isPowerOf.js

--- exercises/numbers/isPowerOf/isPowerOf.js
+++ exercises/numbers/isPowerOf/isPowerOf.js
@@ -1,38 +1,42 @@
 /**
- * Given two non-negative integers, `n` and `b`, return `true` of `n` is a power of `b`
- * and `false` otherwise.
+ * Given a non-negative integer as input, returns `true` if input is a power of `2` and `false` otherwise.
  *
- * A number is a `n` power of `b` if it can be written as `b**k` for some positive integer `k`,
- * where "`**`" represents the exponentiation operator.
+ * A number is a power of `2` if it can be written as `2**k` for some positive integer `k`, where "`**`"
+ * represents the exponentiation operator.
  *
  * For example, since `8` equals `2 * 2 * 2`, which is `2**3`, then `8` is a power of `2`.
- * Conversely, `9` is not a power of `2` since there's no integer `k` such that `9 === 2**k`.
+ * Conversely, `81` is not a power of `2` since there's no integer `k` such that `81 === 2**k`.
+ *
+ * However, `81` _is_ a power of `3` since `81 === 3**4`.
  *
  * @example
- * isPowerOf(2) // => true
- * isPowerOf(3) // => false
- * isPowerOf(4) // => true
- * isPowerOf(2**20) // => true
- * isPowerOf(2**20 + 1) // => false
+ * isPowerOf(2, 2) // => true
+ * isPowerOf(3, 3) // => true
+ * isPowerOf(10, 2) // => false
  *
- * @param {number} num - The input number
- * @returns {boolean} True if input is a power of 2 and false otherwise.
+ * @param {number} n - The number
+ * @param {number} b - The base
+ * @returns {boolean} True if the number is a power of the base, false otherwise.
  */
-function isPowerOf(num) {
+function isPowerOf(n, b) {
   // Your code here
   // Remember, you can assume that num is a positive integer.
+  let counter = 0;
+  while(n > b){;
+    b = b*b;
+    counter++;
+  }
+  return b === n;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for isPowerOf:');
-
-  // Is 0 a power of two? Is 1?
-  console.log(isPowerOf(0) === _____);
-  console.log(isPowerOf(1) === _____);
-
-  console.log(isPowerOf(2) === true);
-  console.log(isPowerOf(3) === false);
-  console.log(isPowerOf(4) === true);
+    // Your sanity checks here.
+  console.log(isPowerOf(2,2) === true);
+  console.log(isPowerOf(3,3) === true);
+  console.log(isPowerOf(10, 2) === false);
+  console.log(isPowerOf(100, 5) === false);
+  console.log(isPowerOf(0, 1) === false);
 }
 
 module.exports = isPowerOf;
