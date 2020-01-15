aebb7905ee7a05071be088fc25640eb76a738aec
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-15 14:52:13 -0800

    Update countLetter.js

--- exercises/strings/countLetter/countLetter.js
+++ exercises/strings/countLetter/countLetter.js
@@ -9,11 +9,24 @@
 
 function countLetter(string, letter) {
   // This is your job. :)
+  let counter=0;
+  for(let i=0; i< string.length; i++){
+    if(string[i] === letter){
+      counter++;
+    }
+  }
+  return counter;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for countLetter:');
-
+  
+  console.log(countLetter('Mississippi', 's') === 4);
+  console.log(countLetter('aaaa','a') === 4);
+  console.log(countLetter('aaaa', 'Q') === 0);
+  console.log(countLetter('AaAaA', 'a') === 2);
+  console.log(countLetter('AaAaA', 'A') ===3);
+  console.log(countLetter('', 'A') === 0);
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
