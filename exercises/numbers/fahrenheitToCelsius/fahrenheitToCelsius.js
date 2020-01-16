fa58786233fabbf184dfbc388900e63894b67a33
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-16 11:32:12 -0800

    Update fahrenheitToCelsius.js

--- exercises/numbers/fahrenheitToCelsius/fahrenheitToCelsius.js
+++ exercises/numbers/fahrenheitToCelsius/fahrenheitToCelsius.js
@@ -6,12 +6,15 @@
  */
 function fahrenheitToCelsius(temp) {
   // This is your job. :).
+  return (temp-32)*5/9;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for fahrenheitToCelsius:');
 
   // Your sanity checks go here.
+  console.log(fahrenheitToCelsius(32) === 0);
+  console.log(fahrenheitToCelsius(98.6) === 37);
 }
 
 module.exports = fahrenheitToCelsius;
