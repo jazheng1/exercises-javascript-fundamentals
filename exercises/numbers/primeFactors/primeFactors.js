f631a1721215a7f2425f565722d343f79eec1a11
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-16 15:55:03 -0800

    Update primeFactors.js

--- exercises/numbers/primeFactors/primeFactors.js
+++ exercises/numbers/primeFactors/primeFactors.js
@@ -13,6 +13,33 @@
  * @param {number} num - A positive integer
  * @returns {number[]} An array of all the prime factors of the given integer
  */
+
+function isPrime(num) {
+  /*
+    Your code goes here.
+  
+    Work out one version that works and don't worry about performance.
+
+    If you're having trouble working it out in code, step out of JS-land
+    and use pen/paper, index cards, etc. — anything that helps you think
+    about it without getting stuck in JavaScript syntax.
+  */
+ if(num <= 1){
+  return false;
+ }
+ else if((num % 3 === 0) && (num > 4)){
+   return false;
+ }else if((num % 5 === 0) && (num > 5)){
+   return false;
+ }else if((num % 2 === 0) && (num > 3)){
+  return false;
+}
+ else{
+   return true;
+ }
+}
+
+
 function primeFactors(num) {
   /*
     Your code goes here.
@@ -23,6 +50,28 @@ function primeFactors(num) {
     and use pen/paper, index cards, etc. — anything that helps you think
     about it without getting stuck in JavaScript syntax.
   */
+ let array= [];
+ let quotient = 0;
+ for(i=2; i <= num; i++){
+   if(num%i === 0){
+     if(isPrime(i) === false){
+      quotient = i
+     }
+     else(isPrime(i) === true)
+      array.push(i);
+   }
+ }
+ return array;
+// for(j=0; j < array.length; j++)
+// {
+//     if((array[j] % 2 == 0) && (array[j] > 2)){
+//       primeArray.push(j);
+//     }else if((array[j] % 3 != 0) && (array[j] > 3)){
+//       primeArray.push(j);
+//     }else((array[j] % 5 != 0) && (array[j] > 5))
+//       primeArray.push(j);
+//     }
+  // return array;
 }
 
 if (require.main === module) {
@@ -30,6 +79,10 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+  console.log(primeFactors(2));
+  console.log(primeFactors(4));
+  console.log(primeFactors(12));
+  // console.log(primeFactors(917329));
 }
 
 module.exports = primeFactors;
