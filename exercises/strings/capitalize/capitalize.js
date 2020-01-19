0bf056962aa0b7fa5b1bc63b8c05fd619db874e5
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-19 13:36:16 -0800

    Update capitalize.js

--- exercises/strings/capitalize/capitalize.js
+++ exercises/strings/capitalize/capitalize.js
@@ -10,8 +10,15 @@
  * @returns {string} A capitalized copy of the string
  */
 
-function capitalize(num) {
+function capitalize(word) {
   // This is your job. :)
+  let firstLetter = word.slice(0,1);
+  console.log(firstLetter);
+  firstLetter = firstLetter.toUpperCase();
+  console.log(firstLetter);
+  let newWord = firstLetter + word.slice(1, word.length);
+  console.log(newWord);
+  return newWord;
 }
 
 if (require.main === module) {
@@ -19,6 +26,10 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+  console.log(capitalize('happy') === 'Happy');
+  console.log(capitalize('') === '');
+  console.log(capitalize('sad') === 'Sad');
+
 }
 
 module.exports = capitalize;
