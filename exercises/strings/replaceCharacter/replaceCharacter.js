bfb6111c7e19868be99e8f4269aa7fd80132e472
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-19 13:35:10 -0800

    Update replaceCharacter.js

--- exercises/strings/replaceCharacter/replaceCharacter.js
+++ exercises/strings/replaceCharacter/replaceCharacter.js
@@ -14,11 +14,20 @@
 
 function replaceCharacter(string, target, replaceWith) {
   // This is your job. :)
+  for(i = 0; i < string.length; i++){
+    if(string[i] === target){
+      string = string.replace(string[i], replaceWith);
+    }
+  }
+  console.log(string);
+  return string;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for replaceCharacter:');
-
+  console.log(replaceCharacter('hello', 'l', '8') === 'he88o');
+  console.log(replaceCharacter('') === '');
+  console.log(replaceCharacter('mississippi', 's', 'i') === 'miiiiiiippi');
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
