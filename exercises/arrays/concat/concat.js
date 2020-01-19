e453fa5401e70d323c5a260da9137bbeab6d4dde
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-19 14:31:33 -0800

    Update concat.js

--- exercises/arrays/concat/concat.js
+++ exercises/arrays/concat/concat.js
@@ -23,11 +23,26 @@
  */
 function concat(leftArray, rightArray) {
   // This is your job. :)
+  let arr = [];
+  for(i=0; i < leftArray.length; i++)
+  {
+    arr.push(leftArray[i]);
+  }
+  for(i=0; i < rightArray.length; i++)
+  {
+    arr.push(rightArray[i]);
+  }
+  console.log(arr);
+  return arr;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for concat:');
-
+  console.log(concat([1, 2, 3], [4, 5, 6]) === [1, 2, 3, 4, 5, 6]);
+  console.log(concat([], []) === []);
+  console.log(concat([-10, undefined], [true, 'waffles']) === [-10, undefined, true, 'waffles']);
+  console.log(concat([20, 104], []) === [20, 104]);
+  console.log(concat([], ['hello', 'world']) === ['hello', 'world']);
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
