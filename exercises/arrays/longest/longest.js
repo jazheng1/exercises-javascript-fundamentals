d008add5d9859fe3afc18b32e5db9a2c43f4c30d
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-19 14:51:58 -0800

    Update longest.js

--- exercises/arrays/longest/longest.js
+++ exercises/arrays/longest/longest.js
@@ -8,11 +8,27 @@
  */
 function longest(array) {
   // This is your job. :)
-}
+  let count = 0;
+  let word ='';
+  let longestWord = '';
+  for(i=0; i < array.length; i++){
+    console.log(array[0])
+    word = array[i];
+    console.log(word)
+    if(count < word.length){
+      count = word.length;
+      longestWord = array[i];
+    }
+    }
+    return longestWord;
+  }
+
 
 if (require.main === module) {
   console.log('Running sanity checks for longest:');
-
+  console.log(longest(['his', 'name', 'is', 'Joseph']) === 'Joseph')
+  console.log(longest(['', '']) === '');
+  console.log(longest(['', 'Peter']) === 'Peter');
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
