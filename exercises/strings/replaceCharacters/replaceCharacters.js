1f9b641c22da8daf2b9d0df6603d24e7efa98e52
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-19 13:34:41 -0800

    Update replaceCharacters.js

--- exercises/strings/replaceCharacters/replaceCharacters.js
+++ exercises/strings/replaceCharacters/replaceCharacters.js
@@ -13,11 +13,30 @@
 
 function replaceCharacters(string, replacementDict) {
   // This is your job. :)
+  for(i = 0; i < string.length; i++)
+  {
+    for(let j of Object.keys(replacementDict)){
+    /**why is it that when I use 
+    (j =0; j < Object.keys(replacementDict).length; j++)
+    if(string[i] === replacementDict[j]){
+      string = string.replace(string[i], replacementDict[j]);
+    }
+    it does not work
+    */
+    console.log(string)
+    if(string[i] === j){
+      string = string.replace(string[i], replacementDict[j]);
+      console.log(string);
+    }
+    }
+  }
+  return string;
+
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for replaceCharacters:');
-
+  console.log(replaceCharacters('hello', { 'h': 'y', 'l': '8' }) === 'ye88o' )
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
