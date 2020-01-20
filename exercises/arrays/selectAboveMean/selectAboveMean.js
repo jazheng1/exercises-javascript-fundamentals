895227e7f0bfde807ce4779a4e048483eb333a23
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-19 17:49:00 -0800

    Update selectAboveMean.js

--- exercises/arrays/selectAboveMean/selectAboveMean.js
+++ exercises/arrays/selectAboveMean/selectAboveMean.js
@@ -16,12 +16,29 @@
  */
 function selectAboveMean(array) {
   // This is your job. :)
+  let newArr = [];
+  let sum = 0;
+  for(i = 0; i< array.length; i++){
+    sum = sum + array[i];
+  }
+  let mean = sum/array.length;
+  for(i = 0; i < array.length; i++){
+    if(array[i] > mean){
+      newArr.push(array[i]);
+    }
+  }
+  console.log(newArr);
+  return newArr;
 }
-
 if (require.main === module) {
   console.log('Running sanity checks for selectAboveMean:');
 
   // Add your own sanity checks here.
+  console.log(selectAboveMean([1, 1, 1]) === []);
+  console.log(selectAboveMean([-10, -10, 0, 10]) === [0, 10]);
+  console.log(selectAboveMean([1, 2, 3, 4, 5]) === [4, 5]);
+  console.log(selectAboveMean([100, 0, 100, 200, 100, 0]) === [100, 100, 200, 100]);
+  
   // How else will you be sure your code does what you think it does?
 }
 
