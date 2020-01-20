8d352eee621ff853ef474e6bdfeebae1f570e10c
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-19 17:43:49 -0800

    Update selectEvens.js

--- exercises/arrays/selectEvens/selectEvens.js
+++ exercises/arrays/selectEvens/selectEvens.js
@@ -15,12 +15,25 @@
  */
 function selectEvens(array) {
   // This is your job. :)
+  let newArr= [];
+  for(i = 0; i < array.length; i++){
+    if(array[i] % 2 === 0){
+      newArr.push(array[i]);
+    }
+  }
+  console.log(newArr);
+  return newArr;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for selectEvens:');
 
   // Add your own sanity checks here.
+  console.log(selectEvens([1, 2, 3, 4, 5])=== [2,4]);
+  console.log(selectEvens([10, 2, 19, 10])=== [10, 2, 10]);
+  console.log(selectEvens([1, 1, 1, 2]) === [2]);
+  console.log(selectEvens([11, 21, 31]) === []);
+  
   // How else will you be sure your code does what you think it does?
 }
 
