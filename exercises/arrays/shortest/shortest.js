d4194e02867103a47a2f1302191cd77bd80b1337
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-19 17:17:00 -0800

    Update shortest.js

--- exercises/arrays/shortest/shortest.js
+++ exercises/arrays/shortest/shortest.js
@@ -8,12 +8,29 @@
  */
 function shortest(array) {
   // This is your job. :)
+  let count = array[0].length;
+  let word ='';
+  let smallestWord = array[0];
+  for(i=0; i < array.length; i++){
+    word = array[i];
+    console.log(word)
+    if(word.length < count){
+      console.log(count)
+      console.log(word)
+      count = word.length;
+      smallestWord = array[i];
+    }
+    }
+    console.log(smallestWord)
+    return smallestWord;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for shortest:');
 
   // Add your own sanity checks here.
+  console.log(shortest(['he', 'has', 'water']) === 'he')
+  console.log(shortest(['Peter', 'likes', 'to', 'drink']) === 'to')
   // How else will you be sure your code does what you think it does?
 }
 
