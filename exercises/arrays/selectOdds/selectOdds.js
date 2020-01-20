2c9412e3be8a727e4df1ed3de0756059d0e9076c
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-19 17:27:04 -0800

    Update selectOdds.js

--- exercises/arrays/selectOdds/selectOdds.js
+++ exercises/arrays/selectOdds/selectOdds.js
@@ -15,10 +15,23 @@
  */
 function selectOdds(array) {
   // This is your job. :)
+  let newArr= [];
+  for(i = 0; i < array.length; i++){
+    if(array[i]%2 > 0){
+      newArr.push(array[i]);
+    }
+  }
+  console.log(newArr);
+  return newArr;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for selectOdds:');
+  console.log(selectOdds([1, 2, 3, 4, 5]) === [1,3,5]);
+  console.log(selectOdds([10, 2, 19, 10, -103]) === [19, -103]);
+  console.log(selectOdds([1, 1, 2, 1]) === [1,1,1]);
+  console.log(selectOdds([10, 20, 30]) === []);
+
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
