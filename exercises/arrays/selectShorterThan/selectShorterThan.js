c1663e97771141f20c1c37995cf9c4a3c53d91da
jazheng1 <59896127+jazheng1@users.noreply.github.com>
2020-01-19 17:23:23 -0800

    Update selectShorterThan.js

--- exercises/arrays/selectShorterThan/selectShorterThan.js
+++ exercises/arrays/selectShorterThan/selectShorterThan.js
@@ -20,11 +20,24 @@
  */
 function selectShorterThan(array, threshold) {
   // This is your job. :)
+  let newArr = [];
+  for(i = 0; i< array.length; i++){
+    if(array[i].length < threshold){
+      newArr.push(array[i]);
+    }
+  }
+  console.log(newArr)
+  return newArr;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for selectShorterThan:');
-
+  console.log(selectShorterThan(['', 'aaa', 'bb', 'c', 'dddd'], 0) === [])
+  console.log(selectShorterThan(['', 'aaa', 'bb', 'c', 'dddd'], 1) === [''])
+  console.log(selectShorterThan(['', 'aaa', 'bb', 'c', 'dddd'], 2) === ['', 'c'])
+  console.log(selectShorterThan(['', 'aaa', 'bb', 'c', 'dddd'], 3) === ['', 'c', 'bb'])
+  console.log(selectShorterThan(['', 'aaa', 'bb', 'c', 'dddd'], 4) === ['', 'c', 'bb', 'aaa'])
+  console.log(selectShorterThan(['', 'aaa', 'bb', 'c', 'dddd'], 5) === ['','c','bb', 'aaa', 'dddd'])
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
